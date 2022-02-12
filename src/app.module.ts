508a99efe51c9c9ee31fe5d9f3faa803b6f6c72f
Will Shepp <28759252+willshepp28@users.noreply.github.com>
2022-02-12 15:24:16 -0500

    included TypeOrmModule and GraphQLModule in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(),
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gpl',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
